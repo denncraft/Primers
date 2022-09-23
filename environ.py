518ed073edbba76328cb370f1455368b7313a82b
denncraft <denrecords@gmai.com>
2022-09-23 20:50:45 +0400

    Добавил новый вариант хранения и получения данных из локальных переменных окружения

--- environ.py
+++ environ.py
@@ -1,2 +1,16 @@
-import os
-print(os.environ['SECRET_KEY'])
+# Вариант№1 простой
+#import os
+
+#print(os.environ['SECRET_KEY'])
+
+# Вариант№2 поинтереснее
+# можно хранить чувствительную информацию в переменных окружения и загружать ее
+# через класс Configuration методом get_env_key
+# при отсутствии в окружении нужного ключа будет возвращен False
+from os import environ
+
+
+class Configuration:
+    @staticmethod
+    def get_env_key(key_name: str):
+        return environ.get(key_name, False)
